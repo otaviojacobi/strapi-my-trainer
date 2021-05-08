a26f867d8c45106a0a1dde242846e865cf7eb1a7
Otavio Jacobi <o.jacobi@sap.com>
2021-05-08 18:54:03 -0300

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '010771099ca4930ddc50f6b6091f8755'),
+    },
+  },
+});
