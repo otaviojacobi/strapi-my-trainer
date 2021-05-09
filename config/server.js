c5f8fb64a770a803d22102232697579a1e7e1ab4
Otavio Jacobi <o.jacobi@sap.com>
2021-05-09 10:21:02 -0300

    adds deployed url

--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: 'https://secure-peak-81279.herokuapp.com',
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '010771099ca4930ddc50f6b6091f8755'),
