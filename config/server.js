63d30af196ea193b3a93964b1355768d922a7449
Otavio Jacobi <o.jacobi@sap.com>
2021-05-09 21:10:41 -0300

    club draft version

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: 'https://secure-peak-81279.herokuapp.com',
+  //url: 'https://secure-peak-81279.herokuapp.com',
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '010771099ca4930ddc50f6b6091f8755'),
